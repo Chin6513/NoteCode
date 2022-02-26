4b9ca21953252e98462d2684227bd50006a8d994
秦 <Chin6513@163.com>
2022-02-26 21:56:11 +0800

    Update stock10.cpp

--- C++/class&obj@stock/stock10.cpp
+++ C++/class&obj@stock/stock10.cpp
@@ -28,3 +28,73 @@ Stock::Stock(const std::string & co, long n, double pr)
 	share_val=pr;
 	set_tot();
 }
+
+//calss destructor
+//析构函数
+Stock::~Stock()	//verbose class destrucotr
+{
+	std::cout<<"Bye, "<<company<<"!\n";
+}
+
+//other methods
+void Stock::buy(long num, double price)
+{
+	if(num<0)
+	{
+		std::cout<<"Number of shares purchased can't be negative. "
+					<<"Transaction is aborted.\n";
+	}
+	else
+	{
+		shares += num;
+		share_val=price;
+		set_tot();
+	}
+}
+
+void Stock::sell(long num ,double price)
+{
+	using std::cout;
+	if(num<0)
+	{
+		cout<<"Number of shares sold can't be negative. "
+			<<"Tansaction is aborted.\n";
+	}
+	else if (num>shares)
+	{
+		cout<<"You can't sell more than you have! "
+			 <<"Transaction is aborted.\n";
+	}
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)
+{
+	share_val = price;
+	set_tot();
+}
+
+void Stock::show()
+{
+	using std::cout;
+	using std::ios_base;
+	//set format to #.###
+	ios_base::fmtflags orig =cout.setf(ios_base::fixed,ios_base::floatfield);
+	std::streamsize prec=cout.precision(3);
+	
+	cout<<"Company: "<<company
+		 <<" Shares: "<<shares <<'\n';
+	cout<<" Share Price: $" <<share_val;
+	//set format to #.##
+	cout.precision(2);
+	cout<<" Total Worth: $"<<total_val<<'\n';
+	
+	//restore original forma
+	cout.setf(orig, ios_base::floatfield);
+	cout.precision(prec);
+}
